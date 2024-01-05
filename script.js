7551ca2ddff5e70a9cae96f80d4f4ebf328b159a
Rodolfo-Sampaio <96917363+Rodolfo-Sampaio@users.noreply.github.com>
2024-01-05 07:57:19 -0300

    Printando tempo na tela

--- script.js
+++ script.js
@@ -9,13 +9,14 @@ const startPauseBt = document.querySelector('#start-pause');
 const musicaFocoInput = document.querySelector('#alternar-musica');
 const iniciarOuPausarBt = document.querySelector('#start-pause span');
 const buttonIcon = document.querySelector('.app__card-primary-button-icon');
+const tempoNaTela = document.querySelector('#timer');
 
 const musica = new Audio('/sons/luna-rise-part-one.mp3');
 const audioPlay = new Audio('/sons/play.wav');
 const audioPausa = new Audio('/sons/pause.mp3');
 const audioTempoFinalizado = new Audio('/sons/beep.mp3');
 
-let tempoDecorridoEmSegundos = 10;
+let tempoDecorridoEmSegundos = 1500;
 let intervaloId = null;
 
 musica.loop = true;
@@ -65,7 +66,7 @@ function alterarContexto(contexto) {
 	}
 }
 
-const contagemRegressica = () => {
+const contagemRegressiva = () => {
 	if (tempoDecorridoEmSegundos <= 0) {
 		// audioTempoFinalizado.play();
 		alert('Tempo Finalizado!');
@@ -73,9 +74,7 @@ const contagemRegressica = () => {
 		return;
 	}
 	tempoDecorridoEmSegundos -= 1;
-	console.log('Temporizador: ' + tempoDecorridoEmSegundos);
-	buttonIcon.src = "/imagens/pause.png"
-	// console.log('Id: ' + intervaloId);
+	mostrarTempo();
 };
 
 startPauseBt.addEventListener('click', iniciarOuPausar);
@@ -87,11 +86,20 @@ function iniciarOuPausar() {
 		return;
 	}
 	audioPlay.play();
-	intervaloId = setInterval(contagemRegressica, 1000);
+	intervaloId = setInterval(contagemRegressiva, 1000);
 	iniciarOuPausarBt.textContent = 'Pausar';
+	buttonIcon.src = '/imagens/pause.png';
 }
 function zerar() {
 	clearInterval(intervaloId);
 	iniciarOuPausarBt.textContent = 'Começar';
+	buttonIcon.src = '/imagens/play_arrow.png';
 	intervaloId = null;
 }
+
+function mostrarTempo() {
+	const tempo = tempoDecorridoEmSegundos;
+	tempoNaTela.innerHTML = `${tempo}`;
+}
+
+mostrarTempo();
