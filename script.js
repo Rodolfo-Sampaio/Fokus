3dedb4932d390ddf69a5aea665f8f036f5b08e31
Rodolfo-Sampaio <96917363+Rodolfo-Sampaio@users.noreply.github.com>
2024-01-05 09:28:21 -0300

    Formatando tempo com objeto Date

--- script.js
+++ script.js
@@ -30,21 +30,25 @@ musicaFocoInput.addEventListener('change', () => {
 });
 
 focoBt.addEventListener('click', () => {
+	tempoDecorridoEmSegundos = 1500;
 	alterarContexto('foco');
 	focoBt.classList.add('active');
 });
 
 curtoBt.addEventListener('click', () => {
+	tempoDecorridoEmSegundos = 300;
 	alterarContexto('descanso-curto');
 	curtoBt.classList.add('active');
 });
 
 longoBt.addEventListener('click', () => {
+	tempoDecorridoEmSegundos = 900;
 	alterarContexto('descanso-longo');
 	longoBt.classList.add('active');
 });
 
 function alterarContexto(contexto) {
+	mostrarTempo();
 	botoes.forEach(function (contexto) {
 		contexto.classList.remove('active');
 	});
@@ -68,7 +72,7 @@ function alterarContexto(contexto) {
 
 const contagemRegressiva = () => {
 	if (tempoDecorridoEmSegundos <= 0) {
-		// audioTempoFinalizado.play();
+		audioTempoFinalizado.play();
 		alert('Tempo Finalizado!');
 		zerar();
 		return;
@@ -98,8 +102,9 @@ function zerar() {
 }
 
 function mostrarTempo() {
-	const tempo = tempoDecorridoEmSegundos;
-	tempoNaTela.innerHTML = `${tempo}`;
+	const tempo = new Date(tempoDecorridoEmSegundos * 1000);
+	const tempoFormatado = tempo.toLocaleTimeString('pt-Br', { minute: '2-digit', second: '2-digit' });
+	tempoNaTela.innerHTML = `${tempoFormatado}`;
 }
 
 mostrarTempo();
