51402fae73de005c4fae1046bf298dfd6c028ed6
Rodolfo-Sampaio <96917363+Rodolfo-Sampaio@users.noreply.github.com>
2024-01-05 07:23:19 -0300

    inserindo áudios no projeto

--- script.js
+++ script.js
@@ -8,8 +8,11 @@ const botoes = document.querySelectorAll('.app__card-button');
 const startPauseBt = document.querySelector('#start-pause');
 const musicaFocoInput = document.querySelector('#alternar-musica');
 const musica = new Audio('/sons/luna-rise-part-one.mp3');
+const audioPlay = new Audio('/sons/play.wav');
+const audioPausa = new Audio('/sons/pause.mp3');
+const audioTempoFinalizado  = new Audio('/sons/beep.mp3');
 
-let tempoDecorridoEmSegundos = 5;
+let tempoDecorridoEmSegundos = 10;
 let intervaloId = null;
 
 musica.loop = true;
@@ -61,21 +64,25 @@ function alterarContexto(contexto) {
 
 const contagemRegressica = () => {
 	if (tempoDecorridoEmSegundos <= 0) {
-		zerar();
+		audioTempoFinalizado.play();
 		alert('Tempo Finalizado!');
+		zerar();
 		return;
 	}
 	tempoDecorridoEmSegundos -= 1;
 	console.log('Temporizador: ' + tempoDecorridoEmSegundos);
+	console.log('Id: ' + intervaloId);
 };
 
 startPauseBt.addEventListener('click', iniciarOuPausar);
 
 function iniciarOuPausar() {
 	if (intervaloId) {
+		audioPausa.play();
 		zerar();
 		return;
 	}
+	audioPlay.play();
 	intervaloId = setInterval(contagemRegressica, 1000);
 }
 function zerar() {
